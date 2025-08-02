c7e8a7249b98f63d5ecb784f85a57a1f37a02f63
Pavan kumar <pavankumarbr07@gmail.com>
2025-08-02 23:10:43 +0530

    Added Day 01: Class and Object example

---
+++ 01_class_and_object/ClassExample.java
@@ -0,0 +1,16 @@
+public class Student {
+    String name;
+    int age;
+
+    void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Pavan";
+        s1.age = 21;
+        s1.displayInfo();
+    }
+}
