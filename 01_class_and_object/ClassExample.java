f3607ab4dff576bcdcc3b5a0f94944ac391d6cdd
Pavankumar B R <162352324+pavankumarbr411@users.noreply.github.com>
2025-08-11 22:55:27 +0530

    Update ClassExample.java

--- 01_class_and_object/ClassExample.java
+++ 01_class_and_object/ClassExample.java
@@ -2,7 +2,7 @@ public class Student {
     String name;
     int age;
 
-    void displayInfo() {
+    void displayInformation() {
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
     }
@@ -11,6 +11,6 @@ public class Student {
         Student s1 = new Student();
         s1.name = "Pavan";
         s1.age = 21;
-        s1.displayInfo();
+        s1.displayInformation();
     }
 }
